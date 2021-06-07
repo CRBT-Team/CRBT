56c2dcd5354637ba4c81ad2a5de10cdcb47ab5df
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-07 22:57:14 +0200

    Update music end.js

--- src/!callbacks/music end.js
+++ src/!callbacks/music end.js
@@ -3,7 +3,7 @@ const { colors, emojis } = require("../../index")
 module.exports.musicEndCommand = {
   channel: "$channelID",
   code: `
-$title[${emojis.general.stop} Inactivity notice]
+$title[${emojis.music.stop} Inactivity notice]
 $description[No music was added to the queue recently so $username[$clientID] left the voice channel.]
 $color[${colors.red}]
 `}
