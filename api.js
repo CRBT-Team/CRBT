433b646f20e49b80a57b21efdc9c94179458de60
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-18 01:21:56 +0200

    Update api.js

--- api.js
+++ api.js
@@ -25,36 +25,50 @@ app.get("/", function (req, res) {
 });
 app.get("/crbt", function (req, res) {
   res.json({
-    online: true,
-    news: botinfo.news,
-    version: {
-      major: botinfo.version,
-      build: botinfo.build,
+    "online": true,
+    "news": botinfo.news,
+    "version": {
+      "major": botinfo.version,
+      "build": botinfo.build,
     },
   });
 });
 app.get("/crbt/stats", function (req, res) {
   res.json({
-    memberCount: instance.memberCount,
-    guildCount: instance.guildCount,
-    commandCount: instance.commandCount,
+    "memberCount": instance.memberCount,
+    "guildCount": instance.guildCount,
+    "commandCount": instance.commandCount,
   });
 });
 
 app.get("/pediluve", function (req, res) {
   res.json({
-    image: pédiluve[Math.floor(Math.random() * (pédiluve.length - 0 + 1))]
+    "image": pédiluve[Math.floor(Math.random() * (pédiluve.length - 0 + 1))]
   })
 })
 
 app.set("json spaces", 2);
 
 app.listen(port, () => {
+
   if (port === "15019") {
+
     console.log(`Connected to the Clembs API (https://api.clembs.xyz)`);
+
   } else {
+
     console.log(`Connected to the Clembs API (http://localhost:${port})`)
+
   };
 }).on("error", () => {
-  console.log("Couldn't connect to Clembs API!");
-});
+
+  app.listen("15019", function() {
+
+    console.log(`Connected to the Clembs API (https://localhost:15019)`);
+
+  }).on("error", () => {
+
+    console.log("Couldn't connect to Clembs API!");
+
+    });
+  });
