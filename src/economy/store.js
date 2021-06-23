5ac04f4ddf2bbdbb98f516b7d048d91fa25ceb93
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-23 22:48:40 +0200

    small changes to make it look good

--- src/economy/store.js
+++ src/economy/store.js
@@ -1,4 +1,4 @@
-const { logos, emojis, items } = require("../../index");
+const { logos, emojis, items, illustrations } = require("../../index");
 const badges = items.badges; const banners = items.banners
 
 module.exports.command = {
@@ -7,7 +7,7 @@ module.exports.command = {
     description_enUS: "Brings the CRBT Store up.",
     module: "economy",
     code: `
-$reactionCollector[$botLastMessageID;$authorID;1h;📛,🏴;badges,banners]
+$reactionCollector[$botLastMessageID;$authorID;1h;${emojis.store.badges},${emojis.store.banners};badges,banners]
 
 $reply[$messageID;
 
@@ -16,21 +16,26 @@ $if[$message==]
     {author:$get[title-$getGlobalUserVar[language]]:${logos.CRBTsmall}}
     {field:$get[purplets-$getGlobalUserVar[language]]:no}
     {field:$get[familiarFaces-$getGlobalUserVar[language]]:
-    ${badges.goodmeal.contents} ${badges.goodmeal.name} - **${emojis.general.purplet} ${badges.goodmeal.value}**
+    ${badges.goodmeal.contents} ${badges.goodmeal.name}\n**${emojis.general.purplet} ${badges.goodmeal.value} Purplets**
     \`$getServerVar[prefix]buy badge goodmeal\`
-    ${badges.udu.contents} ${badges.udu.name} - **${emojis.general.purplet} ${badges.udu.value}**
+    ${badges.udu.contents} ${badges.udu.name}\n**${emojis.general.purplet} ${badges.udu.value} Purplets**
     \`$getServerVar[prefix]buy badge udu\`
     :yes}
     {field:$get[flags-$getGlobalUserVar[language]]:
-    ${badges.france.contents} ${badges.france.name} - **${emojis.general.purplet} ${badges.france.value}**
+    ${badges.france.contents} ${badges.france.name}\n**${emojis.general.purplet} ${badges.france.value} Purplets**
     \`$getServerVar[prefix]buy badge france\`
-    ${badges.usa.contents} ${badges.usa.name} - **${emojis.general.purplet} ${badges.usa.value}**
+    ${badges.usa.contents} ${badges.usa.name}\n**${emojis.general.purplet} ${badges.usa.value} Purplets**
     \`$getServerVar[prefix]buy badge usa\`
-    ${badges.russia.contents} ${badges.russia.name} - **${emojis.general.purplet} ${badges.russia.value}**
+    ${badges.russia.contents} ${badges.russia.name}\n**${emojis.general.purplet} ${badges.russia.value} Purplets**
     \`$getServerVar[prefix]buy badge russia\`
-    ${badges.brazil.contents} ${badges.brazil.name} - **${emojis.general.purplet} ${badges.brazil.value}**
+    ${badges.brazil.contents} ${badges.brazil.name}\n**${emojis.general.purplet} ${badges.brazil.value} Purplets**
     \`$getServerVar[prefix]buy badge brazil\`
     :yes}
+    {field:$get[jobs-$getGlobalUserVar[language]]:
+    ${badges.developer.contents} ${badges.developer.name}\n**${emojis.general.purplet} ${badges.developer.value} Purplets**
+    \`$getServerVar[prefix]buy badge developer\`
+    :yes}
+    {thumbnail:${illustrations.badges}}
     {color:$getGlobalUserVar[color]}
     ;no]
 
