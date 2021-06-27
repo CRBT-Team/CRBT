125be20e11118bdc2cf7689210faa7cf5bc8e6cb
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-28 00:47:41 +0200

    Update misc.js

--- src/variables/misc.js
+++ src/variables/misc.js
@@ -7,33 +7,19 @@ module.exports.variables = {
     r34Query: "", //last query for ()rule34
     webhook_token: "", webhook_id: "", //channel var for every channel
     volume: 50, //default volume so we dont break our ears
-    color: `${colors.crbt.devblue}`, //default border color
+    color: `${colors.lightred.toLowerCase()}`, //default border color
     lastCmd: "", //last command to get exported to awaited cmd
-    avatarlog: "", //the logs for the avatar, will probably change the method if it gets too messy
+    avatarLog: "", //the logs for the avatar, will probably change the method if it gets too messy
     language: "enUS", //default language
     telemetry: "minimal", //two states: minimal or complete 
     activeReminders: "", //to be merged with todo list
     allowedChannels: "", //the allowlist for channels
-    memberExists: "", //for userinfo, because aoi.js sucks
 
     //mod related stuff
     muted_role: "", //note that this one is the only one not in camelCase for compatibility reasons 
     strikelog: "", 
     strikes: 0,
-
-    //to-do list items
-    list1: "",
-    list2: "",
-    list3: "",
-    list4: "",
-    list5: "",
-    list6: "",
-    list7: "",
-    list8: "",
-    list9: "",
-    list10: "",
-
-    //snipe.js vars
+    //snipe stuff
     snipeMsg: "",
 	snipeAuthor: "",
 	snipeChannel: "",
