318346c78fd77d51790648f01dfa5e01722ac147
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-28 00:44:42 +0200

    fixed index.js lol

--- index.js
+++ index.js
@@ -1,87 +1,40 @@
 // Imports
 const { Bot } = require("aoi.js");
 const instance = require("./instance");
-const { readFileSync, readdirSync, statSync, writeFileSync } = require("fs");
+const { readFileSync } = require("fs");
 require("dotenv").config();
 
 // Configuration files
-const { colors, emojis, links, tokens, botinfo, illustrations } = JSON.parse(
-  readFileSync("json/config.json", "utf-8")
-);
+const { colors, emojis, links, tokens, botinfo, illustrations, logos } =
+    JSON.parse(readFileSync("json/config.json", "utf-8"));
 const { items } = JSON.parse(readFileSync("json/store.json", "utf-8"));
 const { jobs } = JSON.parse(readFileSync("json/jobs.json", "utf-8"));
-
-// Export configuration files and instance to be accessed better by commands
-module.exports = {
-  colors,
-  emojis,
-  jobs,
-  links,
-  tokens,
-  botinfo,
-  items,
-  illustrations,
-  instance,
-};
-
-// Command handler variables
-let dir = readdirSync("./src");
-let i = 0;
+const { api } = JSON.parse(readFileSync("json/api.json", "utf-8"));
 
 // Creating the bot
 const bot = new Bot({
-  token: process.env.TOKEN,
-  prefix: ["$getServerVar[prefix]", "<@$clientID>", "<@!$clientID>"],
-  mobile: false,
-  sharding: false,
-  cache: true,
+    token: process.env.TOKEN,
+    prefix: ["$getServerVar[prefix]", "<@$clientID>", "<@!$clientID>"],
+    mobile: false, sharding: false, cache: true,
 });
 
+// Export the bot, configuration files and instance to be accessed better by commands
+module.exports = {
+    bot, colors, emojis, jobs, links, tokens, botinfo, items, logos, illustrations, instance, api,
+};
+
 // Listeners
-bot.onMessage({ guildOnly: false }); //so it can work in DMs, too
-bot.onUserUpdate(); //to fetch username changes
-bot.onInteractionCreate(); //for slash commands and interactions
+bot.onMessage({ guildOnly: false }); // Allow commands to work in DMs
+bot.onUserUpdate(); // Fetch username changes
+bot.onInteractionCreate(); // For slash commands / interactions
+bot.onGuildJoin(); // Send a message when the bot joins a guild
+
+/* Lavalink
+bot.createLavalink("127.0.0.1:2333", "crbt_rewrite", false);
+*/
 
 // Command handler
-handler: while (i < dir.length) {
-  const stat = statSync("./src/" + dir[i]);
+require("./loadCmds")();
 
-  if (stat.isDirectory()) {
-    const readdir = readdirSync("./src/" + dir[i]);
-
-    let nums = 0;
-    while (nums < readdir.length) {
-      dir.push(dir[i] + "/" + readdir[nums]);
-      nums++;
-    }
-    i++;
-    continue handler;
-  } else if (stat.isFile()) {
-    const command = require("./src/" + dir[i]);
-    try {
-      bot[Object.keys(command)[0]](command[Object.keys(command)[0]]);
-      i++;
-      continue handler;
-    } catch (err) {
-      console.error(err.message);
-      delete dir[i];
-
-      continue handler;
-    }
-  } else {
-    console.error("Directory was not a Folder or File");
-    delete dir[i];
-
-    continue handler;
-  }
-}
-
-instance.commandCount = bot.client.bot_commands.size;
-// instance.memberCount = bot.client.members.cache.size // These two aren't working
-// instance.guildCount = bot.client.guilds.cache.size; // Needs $userCount but idk how to access that outside of a command
-
-// Just took the API code from here, put it in another file and required it so it still runs
-require("./api");
-
-// snipe.js stuffs
-bot.onMessageDelete();
+// Start the API
+require("./api/api");
