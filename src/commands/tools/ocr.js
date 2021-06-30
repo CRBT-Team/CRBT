138dd3a45d7168b2841923b617ac6144997558f2
Clembs <67973777+Clembs@users.noreply.github.com>
2021-06-30 22:37:04 +0200

    fixed ocr, still not done with stuff

--- src/commands/tools/ocr.js
+++ src/commands/tools/ocr.js
@@ -5,23 +5,25 @@ module.exports.command = {
     aliases: ["itt", "imagetotext"],
     module: "tools",
     description_enUS: "Retrieves found text inside of an image.",
-    usage_enUS: "{image URL/attachement} (language)",
-    botperms: [""],
+    usage_enUS: "<image URL/attachement>",
     code: `
-    $reply[$messageID;
-    {title:OCR}
-    {color:$getGlobalUserVar[color]}
-    {description:
-\`\`\`
-$jsonRequest[https://api.ocr.space/parse/imageurl?apikey=${tokens.apis.ocr}&url=$replaceText[$replaceText[$checkCondition[$message==];true;$messageAttachement];false;$message]&scale=true&OCREngine=2;ParsedResults#RIGHT#0#LEFT#.ParsedText]
-\`\`\`
-    };no]
-    
+$reply[$messageID;
+{author:OCR - Results}
 
-    $argsCheck[0;{execute:args}]
-    $onlyIf[$getGlobalUserVar[blocklisted]==false;{execute:blocklist}]
-    $onlyIf[$getServerVar[module_$commandInfo[$commandName;module]]==true;{execute:module}]
-    $if[$guildID!=]$onlyIf[$hasPermsInChannel[$channelID;$clientID;embedlinks]==true;{execute:embeds}]$endif
-    $setGlobalUserVar[lastCmd;$commandName]
-    `
+{description:
+\`\`\`
+$jsonRequest[https://api.ocr.space/parse/imageurl?apikey=${tokens.apis.ocr}&url=$get[message]&scale=true&OCREngine=2;ParsedResults#RIGHT#0#LEFT#.ParsedText]\`\`\`
 }
+
+{color:$getGlobalUserVar[color]}
+;no]
+
+$onlyIf[$get[message]!=;{execute:args}]
+
+$let[message;$replaceText[$replaceText[$checkCondition[$message==];false;$message];true;$messageAttachment]]
+
+$onlyIf[$getGlobalUserVar[blocklisted]==false;{execute:blocklist}]
+$onlyIf[$getServerVar[module_$commandInfo[$commandName;module]]==true;{execute:module}]
+$if[$guildID!=]$onlyIf[$hasPermsInChannel[$channelID;$clientID;embedlinks]==true;{execute:embeds}]$endif
+$setGlobalUserVar[lastCmd;$commandName]
+    `}
