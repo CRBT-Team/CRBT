7c945591695a8d388e70717d5b4b3eb23fc7fb33
Clembs <67973777+Clembs@users.noreply.github.com>
2021-07-01 11:20:12 +0200

    Delete disabled directory

--- disabled/flags.js
+++
@@ -1,6 +0,0 @@
-module.exports.variables = {
-    crbtplus: false, 
-    languages: false, //will be deleted when languages are introduced
-    debug: false, //to be renamed to developer-mode later on 
-    accessibility: false //just for legacy purposes, needs to be removed
-}
