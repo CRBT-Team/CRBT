bb4114504297aed7ae24a1d41ea5f8f39812333d
Clembs <67973777+Clembs@users.noreply.github.com>
2021-07-16 01:52:54 +0200

    fuck aoi.js

--- src/commands/tools/paint.js
+++ src/commands/tools/paint.js
@@ -2,20 +2,55 @@ module.exports.command = {
     name: "paint",
     aliases: ["color", "namepaint"],
     code: `
-$if[$get[role]!=undefined]
+
+$djsEval[
+
+if ("$get[role]" === undefined) {
+
+$createRole[$get[color];$get[color];no;no]
+
+$giveRoles[$authorID;$get[role]]
+
+channel.send("debug:\\ncolor:$get[color]\\nrole: $get[role]\\nstat: b")
+
+}
+
+else {
+
+$giveRoles[$authorID;$get[role]]
+
+channel.send("debug:\\ncolor:$get[color]\\nrole: $get[role]\\nstat: a")
+
+}
+]
+
+$onlyIf[$isValidHex[$replaceText[$get[color];#;]]==true;not a real color!!! (use hex only for now, # or not idrc)]
+
+$let[role;$replaceText[$replaceText[$checkCondition[$findRole[$get[color]]==];true;undefined];false;$findRole[$get[color]]]]
+$let[color;#$replaceText[$toUppercase[$message];#;]]
+
+$argsCheck[1;{execute:args}]
+    `}
+
+
+/*
+if ("$get[role]" === "") {
+
+channel.send("painted you in $get[color]")
+
+$giveRoles[$authorID;$findRole[$get[color]]]
 
 $createRole[$get[color];$get[color];no;no]
 
 $onlyIf[$roleCount!=250;role capacity exceeded xd]
 
-$else
+}
 
-$giveRoles[$authorID;$get[color]]
+else {
 
-$endif
+channel.send("painted you in $get[color]")
 
-$let[role;$findRole[$get[color]]]
-$let[color;#$replaceText[$message;#;]]
+$giveRoles[$authorID;$get[role]]
 
-$argsCheck[1;{execute:args}]
-    `}
+}
+*/
--- src/variables/userVars.js
+++ src/variables/userVars.js
@@ -7,6 +7,7 @@ module.exports.variables = {
     color: `${colors.lightred.toLowerCase()}`,
     language: "enUS",
     telemetry: "complete", //two states: minimal or complete 
+    paintedColor: "", //for name color
     
 
 //user related misc data
