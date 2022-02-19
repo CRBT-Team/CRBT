8e4d797b329f42c313fa2b56813bec9222441c12
Clembs <67973777+Clembs@users.noreply.github.com>
2022-02-19 11:47:55 +0100

    11.3.08.5

--- src/modules/fun/lenny.ts
+++ src/modules/fun/lenny.ts
@@ -1,6 +1,6 @@
-import { avatar } from '$lib/functions/avatar';
 import { CRBTError, UnknownError } from '$lib/functions/CRBTError';
-import { TextChannel, Util } from 'discord.js';
+import { webhookSend } from '$lib/functions/webhookSend';
+import { Util } from 'discord.js';
 import { ChatCommand, OptionBuilder } from 'purplet';
 
 export default ChatCommand({
@@ -8,34 +8,18 @@ export default ChatCommand({
   description: 'Appends ( ͡° ͜ʖ ͡°) to your message.',
   options: new OptionBuilder().string('message', 'Your message.'),
   async handle({ message }) {
-    if (this.channel.type !== 'DM') {
-      const content = message
-        ? Util.cleanContent(message.trim(), this.channel) + ' ( ͡° ͜ʖ ͡°)'
-        : '( ͡° ͜ʖ ͡°)';
+    if (this.channel.type === 'DM') {
+      return this.reply(CRBTError('This command cannot be used in DMs'));
+    }
+    const content = message
+      ? Util.cleanContent(message.trim(), this.channel) + ' ( ͡° ͜ʖ ͡°)'
+      : '( ͡° ͜ʖ ͡°)';
 
-      await this.deferReply();
-      try {
-        const webhooks = await ((await this.channel.fetch()) as TextChannel).fetchWebhooks();
-
-        const hook =
-          webhooks.find(
-            (hook) => hook.name === 'CRBT Webhook' && hook.owner.id === this.client.user.id
-          ) ??
-          (await ((await this.channel.fetch()) as TextChannel)
-            .createWebhook('CRBT Webhook')
-            .then((hook) => hook));
-
-        await hook.send({
-          avatarURL: avatar(this.user),
-          username: this.user.username,
-          content,
-        });
-        await this.deleteReply();
-      } catch (e) {
-        await this.editReply(UnknownError(this, String(e)));
-      }
-    } else {
-      this.reply(CRBTError('This command cannot be used in DMs.'));
+    await this.deferReply();
+    try {
+      await webhookSend(this, content).then(() => this.deleteReply());
+    } catch (e) {
+      await this.editReply(UnknownError(this, String(e)));
     }
   },
 });
--- src/modules/fun/reverse.ts
+++ src/modules/fun/reverse.ts
@@ -1,6 +1,7 @@
 import { avatar } from '$lib/functions/avatar';
 import { CRBTError } from '$lib/functions/CRBTError';
-import { TextChannel, Util } from 'discord.js';
+import { webhookSend } from '$lib/functions/webhookSend';
+import { Util } from 'discord.js';
 import { ChatCommand, OptionBuilder } from 'purplet';
 
 export default ChatCommand({
@@ -8,32 +9,21 @@ export default ChatCommand({
   description: 'Reverses your message.',
   options: new OptionBuilder().string('message', 'Your message.', true),
   async handle({ message }) {
-    if (this.channel.type !== 'DM') {
-      const content = Util.cleanContent(message, this.channel).split('').reverse().join('');
+    if (this.channel.type === 'DM') {
+      return this.reply(CRBTError(this, 'This command cannot be used in DMs'));
+    }
+    const content = Util.cleanContent(message, this.channel).split('').reverse().join('');
 
-      await this.deferReply();
-      try {
-        const webhooks = await ((await this.channel.fetch()) as TextChannel).fetchWebhooks();
-
-        const hook =
-          webhooks.find(
-            (hook) => hook.name === 'CRBT Webhook' && hook.owner.id === this.client.user.id
-          ) ??
-          (await ((await this.channel.fetch()) as TextChannel)
-            .createWebhook('CRBT Webhook')
-            .then((hook) => hook));
-
-        await hook.send({
-          avatarURL: avatar(this.user),
-          username: this.user.username.split('').reverse().join(''),
-          content,
-        });
-        await this.deleteReply();
-      } catch (e) {
-        await this.editReply(CRBTError(this, String(e)));
-      }
-    } else {
-      this.reply(CRBTError(this, 'This command cannot be used in DMs.'));
+    await this.deferReply();
+    try {
+      await webhookSend(
+        this,
+        content,
+        this.user.username.split('').reverse().join(''),
+        avatar(this.user)
+      ).then(() => this.deleteReply());
+    } catch (e) {
+      await this.editReply(CRBTError(this, String(e)));
     }
   },
 });
