8a4f55546b0ec4793718e8c0f7073a8459e2367b
Clembs <clembs@clembs.xyz>
2022-02-21 22:25:52 +0100

    without caching

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,7 +1,7 @@
-import Cache from 'node-cache';
+// import Cache from 'node-cache';
 
-export const cache = new Cache({
-  stdTTL: 60 * 60 * 24,
-  checkperiod: 60 * 60 * 24,
-  useClones: false,
-});
+// export const cache = new Cache({
+//   stdTTL: 60 * 60 * 24,
+//   checkperiod: 60 * 60 * 24,
+//   useClones: false,
+// });
--- src/lib/functions/getColor.ts
+++ src/lib/functions/getColor.ts
@@ -1,21 +1,21 @@
-import { cache } from '$lib/cache';
+// import { cache } from '$lib/cache';
 import { colors, db } from '$lib/db';
 import { User } from 'discord.js';
 
 export async function getColor(user: User): Promise<`#${string}`> {
   let result: string;
 
-  if (cache.has(`color_${user.id}`)) {
-    result = cache.get<string>(`color_${user.id}`);
-  } else {
-    const req = await db.profiles.findFirst({
-      where: { id: user.id },
-      select: { crbt_accent_color: true },
-    });
+  // if (cache.has(`color_${user.id}`)) {
+  //   result = cache.get<string>(`color_${user.id}`);
+  // } else {
+  const req = await db.profiles.findFirst({
+    where: { id: user.id },
+    select: { crbt_accent_color: true },
+  });
 
-    result = req && req.crbt_accent_color ? req.crbt_accent_color : `#${colors.default}`;
-    cache.set(`color_${user.id}`, result);
-  }
+  result = req && req.crbt_accent_color ? req.crbt_accent_color : `#${colors.default}`;
+  //   cache.set(`color_${user.id}`, result);
+  // }
 
   result = result === 'profile' ? (await user.fetch()).hexAccentColor : result;
 
