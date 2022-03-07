724e06b02386e46f43ef3976b9bb37da5cd6612f
Clembs <clembs@clembs.xyz>
2022-03-07 18:47:28 +0100

    feat: sample translations + play

--- src/modules/music/play.ts
+++ src/modules/music/play.ts
@@ -1,100 +1,132 @@
-// import { emojis, illustrations } from '$lib/db';
-// import { shortenUrl } from '$lib/util/shortenUrls';
-// import { MessageEmbed, MessageSelectMenu } from 'discord.js';
-// import {
-//   ButtonComponent,
-//   ChatCommand,
-//   components,
-//   OptionBuilder,
-//   row,
-//   SelectMenuComponent,
-// } from 'purplet';
+import { emojis, illustrations } from '$lib/db';
+import {
+  AudioPlayerStatus,
+  createAudioPlayer,
+  createAudioResource,
+  entersState,
+  joinVoiceChannel,
+  VoiceConnectionStatus,
+} from '@discordjs/voice';
+import { MessageEmbed, VoiceChannel } from 'discord.js';
+import {
+  ButtonComponent,
+  ChatCommand,
+  components,
+  OptionBuilder,
+  row,
+  SelectMenuComponent,
+} from 'purplet';
+import ytdl from 'ytdl-core';
 
-// export default ChatCommand({
-//   name: 'play',
-//   description:
-//     'Plays absolutely nothing. This is just a mockup to visualize possible music command control layout.',
-//   options: new OptionBuilder().string('query', 'A URL or title to search', true),
-//   async handle({ query }) {
-//     const tracks = [
-//       {
-//         title: 'Color Rotate Speedrun',
-//         thumb: '',
-//         url: 'https://clembs.xyz/watch/color-rotate-speedrun.mp4',
-//         source: {
-//           name: 'Clembs',
-//           url: 'https://clembs.xyz',
-//         },
-//         addedBy: this.user,
-//         length: '1:00',
-//       },
-//       {
-//         title: 'Never Gonna Give You Up',
-//         thumb: '',
-//         url: 'https://youtube.com/watch?v=dQw4w9WgXcQ',
-//         source: {
-//           name: 'Rick Astley',
-//           url: 'https://youtube.com',
-//         },
-//         addedBy: { id: '327690719085068289', username: 'Clembs' },
-//         length: '3:59',
-//       },
-//     ];
+const player = createAudioPlayer();
 
-//     this.reply({
-//       embeds: [
-//         new MessageEmbed()
-//           .setAuthor({ name: 'Now Playing', iconURL: illustrations.music.information })
-//           .setTitle(tracks[0].title)
-//           .setURL(tracks[0].url)
-//           .setDescription(`\`00:00/${tracks[0].length}\`\nPlaying in <#844903091669303296>`)
-//           .addField('Source', `[${tracks[0].source.name}](${tracks[0].source.url})`, true)
-//           .addField('Added by', `<@!${tracks[0].addedBy.id}>`, true)
-//           .addField('Volume', '100%'),
-//       ],
-//       components: components(
-//         row(
-//           new PlayPause(true).setEmoji(emojis.music.pause).setStyle('PRIMARY'),
-//           new Skip().setEmoji(emojis.music.skip).setStyle('SECONDARY')
-//         ),
-//         row(
-//           new Tracks().addOptions(
-//             tracks.map((track) => ({
-//               label: track.title,
-//               description: `${track.length} • Added by ${track.addedBy.username}`,
-//               value: `${shortenUrl(track.url)}|${track.addedBy.id}`,
-//             }))
-//           )
-//         )
-//       ),
-//     });
-//   },
-// });
+export default ChatCommand({
+  name: 'play',
+  description:
+    'Plays absolutely nothing. This is just a mockup to visualize possible music command control layout.',
+  options: new OptionBuilder().string('query', 'A URL or title to search', true),
+  async handle({ query }) {
+    if (this.user.id !== '327690719085068289') {
+      return this.reply("you're not clembs therefore i don't give a shart about you");
+    }
+    await this.deferReply();
+    const channel = (await this.guild.members.fetch(this.user)).voice.channel as VoiceChannel;
 
-// export const PlayPause = ButtonComponent({
-//   handle(ctx: any) {
-//     console.log(this.component);
-//     this.update({
-//       embeds: this.message.embeds,
-//       components: components(
-//         row(
-//           new PlayPause(!ctx)
-//             .setEmoji(ctx ? emojis.music.play : emojis.music.pause)
-//             .setStyle('PRIMARY'),
-//           new Skip().setEmoji(emojis.music.skip).setStyle('SECONDARY')
-//         ),
-//         row().addComponents(this.message.components[1].components[0] as MessageSelectMenu)
-//       ),
-//     });
-//   },
-// });
-// export const Skip = ButtonComponent({
-//   handle() {},
-// });
+    await playSong(query);
 
-// export const Tracks = SelectMenuComponent({
-//   handle(ctx: null) {
-//     console.log(this.values);
-//     console.log(this.component.options);
-//   },
-// });
+    const connection = await connectToChannel(channel);
+
+    const { videoDetails } = await ytdl.getBasicInfo(query);
+
+    connection.subscribe(player);
+
+    await this.editReply({
+      embeds: [
+        new MessageEmbed()
+          .setAuthor({ name: 'Now Playing', iconURL: illustrations.music.information })
+          .setTitle(videoDetails.title)
+          .setURL(videoDetails.video_url)
+          .setDescription(`\`00:00/${videoDetails.lengthSeconds}\`\nPlaying in ${channel}`)
+          .addField(
+            'Uploaded by',
+            `**[${videoDetails.author.name}](${videoDetails.author.channel_url})**`,
+            true
+          )
+          .addField('Added by', `${this.user}`, true)
+          .addField('Volume', '100%')
+          .setThumbnail(videoDetails.thumbnails.find((t) => t.width === 1920).url),
+      ],
+      components: components(
+        row(
+          new PlayPause(true).setEmoji(emojis.music.pause).setStyle('PRIMARY')
+          // new Skip().setEmoji(emojis.music.skip).setStyle('SECONDARY')
+        )
+        // row(
+        //   new Tracks().addOptions(
+        //     tracks.map((track) => ({
+        //       label: track.title,
+        //       description: `${track.length} • Added by ${track.addedBy.username}`,
+        //       value: `${shortenUrl(track.url)}|${track.addedBy.id}`,
+        //     }))
+        //   )
+        // )
+      ),
+    });
+  },
+});
+
+export const PlayPause = ButtonComponent({
+  handle(ctx: any) {
+    if (ctx) {
+      player.pause();
+    } else {
+      player.unpause();
+    }
+    this.update({
+      embeds: this.message.embeds,
+      components: components(
+        row(
+          new PlayPause(!ctx)
+            .setEmoji(ctx ? emojis.music.play : emojis.music.pause)
+            .setStyle('PRIMARY')
+          // new Skip().setEmoji(emojis.music.skip).setStyle('SECONDARY')
+        )
+        // row().addComponents(this.message.components[1].components[0] as MessageSelectMenu)
+      ),
+    });
+  },
+});
+export const Skip = ButtonComponent({
+  handle() {},
+});
+
+export const Tracks = SelectMenuComponent({
+  handle(ctx: null) {
+    console.log(this.values);
+    console.log(this.component.options);
+  },
+});
+
+const connectToChannel = async (channel: VoiceChannel) => {
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+
+  try {
+    await entersState(connection, VoiceConnectionStatus.Ready, 30000);
+    return connection;
+  } catch (error) {
+    connection.destroy();
+    throw error;
+  }
+};
+
+const playSong = (query: string) => {
+  const stream = ytdl(query, { filter: 'audioonly' });
+  const resource = createAudioResource(stream);
+  player.play(resource);
+
+  return entersState(player, AudioPlayerStatus.Playing, 30000);
+};
