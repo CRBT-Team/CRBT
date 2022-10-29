b58eae919852089b304f229f0bf43f8801acaba7
Clembs <clembs@clembs.com>
2022-10-30 01:10:10 +0200

    fix: optimise color fetching

--- src/lib/functions/getColor.ts
+++ src/lib/functions/getColor.ts
@@ -1,39 +1,25 @@
-import { cache } from '$lib/cache';
+import { fetchWithCache } from '$lib/cache';
 import { prisma } from '$lib/db';
 import { colors } from '$lib/env';
 import { Guild, User } from 'discord.js';
 
 export async function getColor(thing: User | Guild) {
   const isUser = thing instanceof User;
-  const fromCache = cache.get<number | 'profile'>(`${thing.id}:color`);
-
-  if (fromCache) {
-    if (fromCache === 'profile' && isUser) {
-      return (await thing.fetch()).accentColor ?? colors.default;
-    }
-    return fromCache as number;
-  }
 
   const query = {
     where: { id: thing.id },
     select: { accentColor: true },
   };
 
-  const preferences = isUser
-    ? await prisma.user.findFirst(query)
-    : await prisma.servers.findFirst(query);
+  const accentColor = await fetchWithCache(`${thing.id}:color`, () =>
+    (isUser ? prisma.user.findFirst(query) : prisma.servers.findFirst(query)).then(
+      (t) => t?.accentColor
+    )
+  );
 
-  if (preferences && preferences.accentColor) {
-    const color = preferences.accentColor;
-    cache.set(`${thing.id}:color`, color);
-
-    if (color === 0 && isUser) {
-      return (await thing.fetch()).accentColor ?? colors.default;
-    }
-
-    return typeof color === 'string' ? parseInt(color, 16) : color;
+  if (accentColor === 0 && isUser) {
+    return (await thing.fetch()).accentColor ?? colors.default;
   }
 
-  cache.set(`${thing.id}:color`, colors.default);
-  return colors.default;
+  return accentColor ?? colors.default;
 }
