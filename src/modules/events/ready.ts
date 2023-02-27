11d30479628f89ef237bedb826e3359974273f81
Clembs <67973777+Clembs@users.noreply.github.com>
2023-02-27 02:24:53 +0100

    fix: dev bot readies with economy cmds saved

--- src/modules/events/ready.ts
+++ src/modules/events/ready.ts
@@ -2,8 +2,11 @@ import { prisma } from '$lib/db';
 import { clients, servers } from '$lib/env';
 import { dbTimeout } from '$lib/timeouts/dbTimeout';
 import { AnyTimeout, TimeoutTypes } from '$lib/types/timeouts';
-import { ApplicationCommand, Collection } from 'discord.js';
-import { OnEvent } from 'purplet';
+import { Routes } from 'discord-api-types/v10';
+import { ApplicationCommand, Client, Collection } from 'discord.js';
+import { getRestClient, OnEvent } from 'purplet';
+import { getSettings } from '../settings/serverSettings/_helpers';
+import { economyCommands } from './interaction';
 
 export let allCommands: Collection<string, ApplicationCommand>;
 
@@ -13,15 +16,46 @@ export default OnEvent('ready', async (client) => {
     type: 'WATCHING',
   });
 
-  allCommands = await client.application.commands.fetch({
-    guildId: client.user.id !== clients.crbt.id ? servers.community : undefined,
+  await Promise.all(await loadEconomyCommands(client)).then(async () => {
+    allCommands = await client.application.commands.fetch({
+      guildId: client.user.id !== clients.crbt.id ? servers.community : undefined,
+    });
+
+    console.log(`Loaded ${allCommands.size} commands`);
   });
 
-  console.log(`Loaded ${allCommands.size} commands`);
+  loadTimeouts();
+});
 
+function loadTimeouts() {
   Object.values(TimeoutTypes).forEach((type) =>
     (prisma[type as string].findMany() as Promise<AnyTimeout[]>).then((timeouts) =>
       timeouts.map((t) => dbTimeout(type, t, true))
     )
   );
-});
+}
+
+async function loadEconomyCommands(client: Client): Promise<Promise<any>[]> {
+  const promises: Promise<any>[] = [];
+
+  if (client.user.id !== clients.crbt.id) {
+    const { modules, economy } = await getSettings(servers.community);
+
+    if (modules.economy) {
+      Object.values(economyCommands).map((command) => {
+        const commandMeta = command.getMeta({
+          plural: economy.currencyNamePlural,
+          singular: economy.currencyNameSingular,
+        });
+
+        promises.push(
+          getRestClient().post(
+            Routes.applicationGuildCommands(client.application.id, servers.community),
+            { body: commandMeta }
+          )
+        );
+      });
+    }
+  }
+  return promises;
+}
