5addf5522175d3dd1ea31df7a9fe47176d021aa9
Clembs <clembs@clembs.com>
2023-04-15 03:13:56 +0200

    feat: consider channel activity and welcome screen

--- src/modules/settings/serverSettings/analyze/_helpers.ts
+++ src/modules/settings/serverSettings/analyze/_helpers.ts
@@ -1,6 +1,13 @@
 import { JoinLeaveData } from '$lib/types/messageBuilder';
 import dayjs from 'dayjs';
-import { APIChannel, ChannelType, PermissionFlagsBits, Routes } from 'discord-api-types/v10';
+import {
+  APIChannel,
+  APIGuildWelcomeScreen,
+  APIMessage,
+  ChannelType,
+  PermissionFlagsBits,
+  Routes,
+} from 'discord-api-types/v10';
 import { Interaction } from 'discord.js';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
 import { getRestClient } from 'purplet';
@@ -17,13 +24,25 @@ export async function generateServerInfo({ guild }: Interaction): Promise<string
   const members = await guild.members.fetch();
   const roles = await guild.roles.fetch();
   const serverRole = roles.get(guild.id);
-  const channels = guild.channels.cache
-    .filter(
-      (c) =>
-        'permissionsFor' in c && c.permissionsFor(serverRole)?.has(PermissionFlagsBits.ViewChannel)
-    )
-    .map((c) => c.id);
-  const apiChannels = (await getRestClient().get(Routes.guildChannels(guild.id))) as APIChannel[];
+
+  const apiPublicChannels = (
+    (await getRestClient().get(Routes.guildChannels(guild.id))) as APIChannel[]
+  ).filter((c) => {
+    if (!('permission_overwrites' in c)) return true;
+    const permOverwrites = c.permission_overwrites.find((o) => o.id === guild.id);
+    if (!permOverwrites) return true;
+    const perms = BigInt(permOverwrites.deny);
+
+    return (
+      !((perms & PermissionFlagsBits.ViewChannel) === PermissionFlagsBits.ViewChannel) &&
+      (c.type === ChannelType.GuildVoice
+        ? !((perms & PermissionFlagsBits.Connect) === PermissionFlagsBits.Connect)
+        : true)
+    );
+  });
+
+  console.log(apiPublicChannels.map(({ type, name, id }) => `${type} ${name} - ${id}`));
+
   const bots = members.filter(({ user }) => user.bot);
   const channelType = {
     [ChannelType.GuildAnnouncement]: 'Announcement',
@@ -31,37 +50,109 @@ export async function generateServerInfo({ guild }: Interaction): Promise<string
     [ChannelType.GuildVoice]: 'Voice',
     [ChannelType.GuildStageVoice]: 'Stage',
   };
+  async function analyzeChannelActivity(channel: APIChannel) {
+    if (
+      channel.type !== ChannelType.GuildText &&
+      channel.type !== ChannelType.GuildAnnouncement &&
+      channel.type !== ChannelType.GuildVoice
+    )
+      return;
+    if (channel.id === guild.rulesChannelId) return;
+
+    const permOverwrites = channel.permission_overwrites.find((c) => c.id === guild.id);
+    const isReadOnly = permOverwrites
+      ? (BigInt(permOverwrites.deny) & PermissionFlagsBits.SendMessages) ===
+        PermissionFlagsBits.SendMessages
+      : false;
+
+    const messages = (await getRestClient().get(Routes.channelMessages(channel.id), {
+      query: new URLSearchParams({
+        limit: isReadOnly ? '5' : '25',
+      }),
+    })) as APIMessage[];
+
+    console.log(channel.name, messages.length);
+
+    if (messages.length) {
+      const firstMessageDate = new Date(messages.at(-1).timestamp);
+      const lastMessageDate = new Date(messages[0].timestamp);
+
+      return {
+        id: channel.id,
+        firstLastMsgDistance: Math.round(
+          (lastMessageDate.getTime() - firstMessageDate.getTime()) / 1000 / 60 / 60
+        ),
+        lastMessageDate,
+      };
+    }
+  }
+  const channelsWithActivity = (
+    await Promise.all(
+      apiPublicChannels
+        .filter((c) => c.type !== ChannelType.GuildCategory)
+        .map(async (c) => await analyzeChannelActivity(c))
+    )
+  ).filter(Boolean);
+  console.log(channelsWithActivity);
+
+  function formatChannelName(
+    c: Partial<{
+      name: string;
+      id: string;
+      type: ChannelType;
+    }>,
+    includeActivity = true
+  ) {
+    const activity = !includeActivity ? null : channelsWithActivity.find((cA) => cA?.id === c?.id);
+
+    if (!c) return;
+
+    return `- ${c.name} ${
+      channelType?.[c.type]
+        ? `(${channelType[c.type]})`
+        : c.id === guild.rulesChannelId
+        ? '(Rules channel)'
+        : !serverRole.permissionsIn(c.id).has(PermissionFlagsBits.SendMessages)
+        ? '(Read-only)'
+        : ''
+    }${
+      activity
+        ? ` - ${activity.firstLastMsgDistance}m - Last msg ${dayjs(
+            activity.lastMessageDate
+          ).fromNow()}`
+        : ''
+    }`;
+  }
   const crbtSettings = await getSettings(guild.id);
+
+  const welcomeScreen = !guild.features.includes('WELCOME_SCREEN_ENABLED')
+    ? null
+    : ((await getRestClient().get(Routes.guildWelcomeScreen(guild.id))) as APIGuildWelcomeScreen);
 
   return `
 ${guild.name} - SERVER INFO
-  
+    
 Owner: ${owner.user.username}
 ${members.size - bots.size} members + ${bots.size} bots
 Description: ${guild.description}
-Created ${dayjs(guild.createdAt).fromNow(true)}
+Language: ${guild.preferredLocale}
 
-PUBLIC CHANNELS
+Created ${dayjs(guild.createdAt).fromNow()}
 
-${apiChannels
-  .filter((c) => c.type === ChannelType.GuildCategory && channels.includes(c.id))
+PUBLIC CHANNELS (format "channel name (type) - Activity")
+${apiPublicChannels
+  .filter((c) => 'parent_id' in c && !c.parent_id && c.type !== ChannelType.GuildCategory)
+  .map((c) => formatChannelName(c))
+  .join('\n')}
+  
+${apiPublicChannels
+  .filter((c) => c.type === ChannelType.GuildCategory)
   .map(
     (category) =>
       `${category.name}
-${apiChannels
-  .filter((c) => 'parent_id' in c && c.parent_id === category.id && channels.includes(c.id))
-  .map(
-    (c) =>
-      `- ${c.name} ${
-        channelType?.[c.type]
-          ? `(${channelType[c.type]})`
-          : c.id === guild.rulesChannelId
-          ? '(Rules channel)'
-          : !serverRole.permissionsIn(c.id).has(PermissionFlagsBits.SendMessages)
-          ? '(Read-only)'
-          : ''
-      }`
-  )
+${apiPublicChannels
+  .filter((c) => 'parent_id' in c && c.parent_id === category.id)
+  .map((c) => formatChannelName(c))
   .join('\n')}`
   )
   .join('\n\n')}
@@ -80,7 +171,28 @@ ${guild.roles.cache
   .map((r) => `- ${r.name} - ${r.members.size}`)
   .join('\n')}
 
-BOT FEATURES
+WELCOME SCREEN
+
+${
+  guild.features.includes('WELCOME_SCREEN_ENABLED')
+    ? `
+Description: ${welcomeScreen.description}
+
+Channels: 
+${welcomeScreen.welcome_channels
+  .map(
+    (c) =>
+      `${formatChannelName(
+        apiPublicChannels.find(({ id }) => id === c.channel_id),
+        false
+      )}:\n${c.description}`
+  )
+  .join('\n')}
+`
+    : 'Disabled'
+}
+
+CRBT BOT FEATURES
 
 Welcome message: 
 ${
@@ -108,7 +220,7 @@ export async function createAnalysisMessage(this: Interaction) {
 
   const chatCompletion = await openai.createChatCompletion({
     frequency_penalty: 0.7,
-    temperature: 0.5,
+    temperature: 0.2,
     messages: [
       systemMessage,
       {
@@ -130,9 +242,9 @@ export const systemMessage: ChatCompletionRequestMessage = {
 
 Your first message to the user should be a short letter describing what they have done well and an overview of what they can improve. This should be structured as a paragraph of good things, followed by a numbered list of potential flaws.
 
-Start with "Hello {owner}", do not sign your name at the end nor introduce yourself. Make sure to bold key points and sentences.
+Start with "Hello {owner}", make sure to bold key points and sentences.
 
-End with a positive note, encouraging the user to make use of your suggestions.
+End with a positive note, encouraging the user to make use of your suggestions, and do not sign your name.
 
 Generally, servers tend to have some "general" channels like:
 - general/main/chat/hangout - these are general topic channels where members discuss about everything on general-purpose servers, or stay on topic with the server's identity, if any
@@ -140,6 +252,10 @@ Generally, servers tend to have some "general" channels like:
 - create/creativity/design/art - most general-purpose servers use one of these in order to have people promote their content, share their art or progress on their projects
 - Channels marked as "Rules" host rules about the server, welcome messages explaining the server in-depth, or other important information
 - Forum channels are a new type of channel where members may post media or create sub-channels matching more specific topics
+
+The "activity" property defines the interval in minutes between the last and the 25th last message posted within this channeL. The higher the number is, the less active it is. The lower, the more active it is.
+
+The "CRBT BOT FEATURES" section refers to features the user has set up with the CRBT Discord bot. These features may be used on other bots instead, so always consider this possibility.
 `,
 
   // At the end of each message should be a list of up to 3 buttons that are placed as suggestions for what the server owner can say as follow up questions. These suggestions must be under 80 characters as per Discord's limits, so keep them very concise but descriptive enough. Format these options at the end of your message on separate lines starting with "---"
