dc0348b0fb5e767ede51bbe498c05076ad72e417
Clément Voisin <clembs@clembs.com>
2023-08-14 15:41:54 +0200

    fix: share result behavior on ctx cmds

--- src/modules/components/ShareResult.ts
+++ src/modules/components/ShareResult.ts
@@ -3,11 +3,12 @@ import { slashCmd } from '$lib/functions/commandMention';
 import { userMention } from '@purplet/utils';
 import { Channel, Message, MessageInteraction, MessageOptions } from 'discord.js';
 import { ButtonComponent, row } from 'purplet';
+import { allCommands } from '../events/ready';
 
 export function ShareResponseBtn(i: { locale: string; channel: Channel }, compact = true) {
   return new ShareResponseBtnShell()
     .setEmoji(emojis.buttons.preview)
-    .setLabel(compact ? '' : 'Share result')
+    .setLabel(compact ? '' : 'Share response')
     .setStyle('SECONDARY')
     .setDisabled(i.channel.type === 'DM');
 }
@@ -16,19 +17,27 @@ export const ShareResponseBtnShell = ButtonComponent({
   async handle() {
     const { content, embeds, attachments, components } = this.message as Message;
     const commandName = (this.message.interaction as MessageInteraction).commandName;
+    const command = allCommands.find((c) => c.name === commandName);
 
     const newMessage: MessageOptions = {
-      content: `${userMention(this.user.id)} used ${slashCmd(
-        commandName
-      )} and shared its result.\n\n${content}`,
+      content: `${userMention(this.user.id)} used ${
+        command.type === 'CHAT_INPUT'
+          ? slashCmd(commandName)
+          : `the **${commandName}** context menu command`
+      } and shared its response.\n\n${content}`,
       embeds,
       attachments: Array.from(attachments.values()),
       components: components
         .filter((r) => r.components.filter((c) => c.customId !== this.customId).length)
         .map((r) =>
           row().addComponents(
-            r.components.filter((b) => b.customId !== this.customId).map((b) => b.setDisabled())
-          )
+            r.components
+
+              .filter((b) => b.customId !== this.customId)
+              .map((b) =>
+                b.type === 'BUTTON' && b.style === 'LINK' ? b : ({ ...b, disabled: true } as any),
+              ),
+          ),
         ),
     };
 
@@ -37,8 +46,8 @@ export const ShareResponseBtnShell = ButtonComponent({
     await this.update({
       components: components.map((r) =>
         row().addComponents(
-          r.components.map((b) => (b.customId === this.customId ? b : b.setDisabled()))
-        )
+          r.components.map((b) => (b.customId !== this.customId ? b : b.setDisabled())),
+        ),
       ),
     });
   },
