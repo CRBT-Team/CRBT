7004cf86891474ef2529f1dc87b73c593c321247
Clément Voisin <clembs@clembs.com>
2024-02-11 12:44:26 +0100

    chore: rewrite imgDominantColor to use extract-colors

--- src/lib/functions/imgDominantColor.ts
+++ src/lib/functions/imgDominantColor.ts
@@ -1,15 +1,22 @@
-import getColors from 'get-image-colors';
+import chroma from 'chroma-js';
+import { extractColors } from 'extract-colors';
+import { FinalColor } from 'extract-colors/lib/types/Color';
+import getPixels from 'get-pixels';
 import { fetch } from 'undici';
 
 export async function imgDominantColor(imageUrl: string) {
   const buffer = await (await fetch(imageUrl)).arrayBuffer();
+  let averageColor: FinalColor;
 
-  const averageColor = (
-    await getColors(Buffer.from(buffer), {
-      type: 'image/png',
-      count: 1,
+    getPixels(Buffer.from(buffer), 'image/png', async (err, pixels) => {
+      if (!err) {
+        const data = [...pixels.data];
+        const width = Math.round(Math.sqrt(data.length / 4));
+        const height = width;
+
+        averageColor = (await extractColors({ data, width, height }))?.[0];
+      }
     })
-  )?.[0];
 
-  return averageColor;
+  return chroma(averageColor.hex);
 }
