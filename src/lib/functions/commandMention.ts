21e0918387bd387c47be5e4cd2f6ef0792ac0640
Clément Voisin <clembs@clembs.com>
2024-07-17 16:23:38 +0200

    fix: dont wait for all cmds to load to use commandMention

--- src/lib/functions/commandMention.ts
+++ src/lib/functions/commandMention.ts
@@ -3,7 +3,11 @@ import { allCommands } from '../../modules/events/ready';
 export function slashCmd(commandName: string) {
   const rootCmdName = commandName.split(' ')[0];
 
-  const command = allCommands.find((c) => c.name === rootCmdName);
+  if (allCommands && allCommands.size) {
+    const command = allCommands.find((c) => c.name === rootCmdName);
 
-  return `</${commandName}:${command?.id}>`;
+    return `</${commandName}:${command?.id}>`;
+  } else {
+    return `/${commandName}`;
+  }
 }
