4d70ebaf8b6c43c72faac5ddd3f450b7570b86a5
Clément Voisin <clembs@clembs.com>
2024-07-18 22:10:33 +0200

    feat: push economy commands to all servers

--- src/modules/events/ready.ts
+++ src/modules/events/ready.ts
@@ -2,11 +2,11 @@ import { prisma } from '$lib/db';
 import { clients, servers } from '$lib/env';
 import { dbTimeout } from '$lib/timeouts/dbTimeout';
 import { AnyTimeout, TimeoutTypes } from '$lib/types/timeouts';
-import { Routes } from 'discord-api-types/v10';
+import { APIApplicationCommand, Routes } from 'discord-api-types/v10';
 import { ApplicationCommand, Client, Collection } from 'discord.js';
 import { OnEvent, getRestClient } from 'purplet';
 import { economyCommands } from '../economy/_helpers';
-import { getGuildSettings } from '../settings/server-settings/_helpers';
+import { fetch } from 'undici';
 
 export let allCommands: Collection<string, ApplicationCommand>;
 
@@ -44,25 +44,58 @@ function loadTimeouts(client: Client) {
 }
 
 async function loadEconomyCommands(client: Client) {
-  const { modules, economy } = await getGuildSettings(servers.community);
+  if (client.user.id !== clients.crbt.id) {
+    const guilds = await prisma.guild.findMany({
+      where: {
+        modules: {
+          economy: true,
+        },
+      },
+      include: { economy: { include: { items: true } } },
+    });
 
-  if (client.user.id !== clients.crbt.id && modules.economy) {
     return await Promise.all(
-      Object.entries(economyCommands).map(async ([name, command]) => {
-        if (!economy.categories.length && name === 'shop') return;
+      guilds.map(async (guild) => {
+        const discordGuild = await client.guilds.fetch(guild.id);
+        const urlParams = new URLSearchParams();
+        urlParams.set('with_localizations', 'true');
 
-        console.log(`Fetching ${name}`);
+        const commands = (await getRestClient().get(
+          Routes.applicationGuildCommands(client.user.id, guild.id),
+          {
+            query: urlParams,
+          },
+        )) as Partial<APIApplicationCommand>[];
 
-        const commandMeta = command.getMeta({
-          plural: economy.currencyNamePlural,
-          singular: economy.currencyNameSingular,
+        Object.entries(economyCommands).forEach(([name, command]) => {
+          if (!guild.economy.items.length && name === 'shop') return;
+
+          commands.push({
+            ...command.getMeta({
+              plural: guild.economy.currencyNamePlural,
+              singular: guild.economy.currencyNameSingular,
+            }),
+            guild_id: guild.id,
+            default_member_permissions: null,
+            type: 1,
+            application_id: client.user.id,
+            nsfw: false,
+          });
         });
 
-        await getRestClient().post(
-          Routes.applicationGuildCommands(client.application.id, servers.community),
-          { body: commandMeta },
+        await fetch(
+          `https://discord.com/api/v10/applications/${client.user.id}/guilds/${guild.id}/commands`,
+          {
+            method: 'PUT',
+            headers: {
+              Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(commands),
+          },
         );
-        console.log(`Posted ${name}`);
+
+        console.log(`Posted ${commands.length} on ${discordGuild.name ?? guild.id}`);
       }),
     );
   }
