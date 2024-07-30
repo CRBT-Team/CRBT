749df9212a653ecd6c9f6a33e088f3264cdfde34
Clément Voisin <clembs@clembs.com>
2024-07-30 17:19:29 +0200

    fix: work cooldown issues + level up message

--- src/modules/economy/work.ts
+++ src/modules/economy/work.ts
@@ -1,4 +1,3 @@
-import { prisma } from '$lib/db';
 import { colors, emojis } from '$lib/env';
 import { CooldownError } from '$lib/functions/CRBTError';
 import { t } from '$lib/language';
@@ -6,7 +5,7 @@ import dedent from 'dedent';
 import { components, row } from 'purplet';
 import { RemindButton } from '../components/RemindButton';
 import { getGuildSettings } from '../settings/server-settings/_helpers';
-import { EconomyCommand, currencyFormat, upsertGuildMember } from './_helpers';
+import { EconomyCommand, currencyFormat, getServerMember, upsertGuildMember } from './_helpers';
 
 export const work: EconomyCommand = {
   getMeta({ plural }) {
@@ -16,23 +15,21 @@ export const work: EconomyCommand = {
     };
   },
   async handle() {
-    const userData = await prisma.guildMember.findFirst({
-      where: {
-        AND: {
-          userId: this.user.id,
-          guildId: this.guildId,
-        },
-      },
-      select: { workExp: true, lastWork: true },
-    });
+    const memberData = await getServerMember(this.user.id, this.guildId);
 
-    const level = userData?.workExp ? Math.round(Math.log(userData?.workExp / 100)) : 0;
+    function calculateLevel(workExp?: number) {
+      if (!workExp) return 0;
+
+      return Math.round(Math.log(workExp / 100));
+    }
+
+    const level = calculateLevel(memberData?.workExp);
 
     const cooldown = 60 * 1000 * 5 + level * 0.1;
-    const lastWork = (userData?.lastWork?.getTime() || Date.now()) - cooldown;
+    const lastWork = memberData?.lastWork?.getTime() || Date.now();
     const timeDiff = Date.now() - lastWork;
 
-    if (timeDiff && timeDiff < cooldown) {
+    if (timeDiff && cooldown > timeDiff) {
       return this.reply(await CooldownError(this, Date.now() + cooldown, true));
     }
 
@@ -82,7 +79,7 @@ export const work: EconomyCommand = {
       },
     );
 
-    const newLevel = Math.round(Math.log(newData.workExp / 100));
+    const newLevel = calculateLevel(newData.workExp);
 
     await this.reply({
       embeds: [
@@ -94,9 +91,14 @@ export const work: EconomyCommand = {
           fields: [
             {
               name: 'Experience Points earned',
-              value: `+${expGain} (${newData.workExp} total, level ${newLevel})`,
+              value:
+                `+${expGain} (${newData.workExp} total, level ${newLevel})` +
+                (newLevel > level ? ` **[LEVEL UP!]**` : ''),
             },
           ],
+          footer: {
+            text: 'Use the button below to know when you can work again as early as possible.',
+          },
           color: colors.success,
         },
       ],
